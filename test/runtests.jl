366bec927a026f0f967b7245b2e34c415006ff9c
Lukas Reich <lukasreich@online.de>
2026-02-09 10:44:27 +0100

    Update runtests.jl
    #6

---
+++ test/runtests.jl
@@ -0,0 +1,21 @@
+using GfPEPS
+using Test
+
+@testset "GfPEPS tests" begin
+    tests = [
+        "test_Bogoliubov.jl",
+        # "test_energy_BCS_cm.jl",
+        # "test_energy_BCS_translate.jl",
+        # "test_energy_kitaev.jl",
+        # "test_energy_PEPS.jl",
+        # "test_env_init.jl",
+        # "test_hole_density_BCS.jl",
+        # "test_hole_density_BCS_GW.jl",
+        # "test_kitaev_cm_energy.jl",
+        # "test_optimal_gap_param.jl",
+    ]
+    @time for t in tests
+        println("\nRunning test file: $(t)")
+        @time include(t)
+    end
+end;
